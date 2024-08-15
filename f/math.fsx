6a223550495b410806c011cc9d1dd8aae38650a0
Charlie <charlienino@gmail.com>
2024-08-14 20:47:42 -0700

    Chapter 5

--- f/math.fsx
+++ f/math.fsx
@@ -38,9 +38,11 @@ type Name = Name of string
 type Person = {First:string; Last:string}
 
 let me = {First="Charlie"; Last="Woudlntyouliketoknow"}
+let wife = {First="Jen"; Last="Woudlntyouliketoknow"}
 
 let {First=first; Last=last} = me // This is the same as below
-
+let firstWife = wife.First
+let lastWife = wife.Last
 
 type OrderQuantity =
 | UnitQuantity of int
@@ -54,9 +56,36 @@ let x = typedefof<OrderQuantity>
 let printQuantity aOrderQty =
     match aOrderQty with
     | UnitQuantity uQty ->
-      printfn "%i units" uQty
+        printfn "%i units" uQty
     | KilogramQuantity kgQty ->
-      printfn "%g kg" kgQty
+    printfn "%g kg" kgQty
 
 printQuantity anOrderQtyInUnits
 printQuantity anOrderQtyInKg
+
+
+// define some types
+type CustomerId = CustomerId of int
+type OrderId = OrderId of int
+
+// define some values
+let customerId = CustomerId 42
+let orderId = OrderId 42
+
+// try to compare them -- compiler error!
+printfn "%b" (orderId = customerId)
+//                      ^ This expression was expected to
+//                      have type 'OrderId'
+
+
+// construct
+let customerId = CustomerId 42
+// deconstruct
+let (CustomerId innerValue) = customerId
+//              ^ innerValue is set to 42
+printfn "%i" innerValue // prints "42"
+
+// deconstruct
+let processCustomerId (CustomerId innerValue) = printfn "innerValue is %i" innerValue
+// function signature
+// val processCustomerId: CustomerId -> unit
